60b0b79002b389946fccc129f90309bc0757bc73
shenheng <ss5205201001@outlook.com>
2022-11-10 16:32:19 +0800

    feat:第三章核心内容学习

---
+++ os3/src/task/switch.rs
@@ -0,0 +1,8 @@
+use crate::task::task_context::TaskContext;
+core::arch::global_asm!(include_str!("switch.S"));
+
+extern "C" {
+    /// Switch to the context of `next_task_cx_ptr`, saving the current context
+    /// in `current_task_cx_ptr`.
+    pub fn __switch(current_task_cx_ptr: *mut TaskContext, next_task_cx_ptr: *const TaskContext);
+}
